fcfc08acc074dc61095b6b45a79d24befb958afd
dyik <51643039+dyik@users.noreply.github.com>
2019-12-08 16:11:35 +0700

    Update call.php

--- call.php
+++ call.php
@@ -1,10 +1,10 @@
 <?php
-// Limit 3x Telpon Setiap Satu Nomor
+// 
 function send($phone){
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, "https://telegram.org/");                      curl_setopt($ch, "https://t.me/telepulsa22/", POST);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, client_id);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, redirect_uri);
         curl_setopt($ch, CURLOPT_HEADER, true);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
